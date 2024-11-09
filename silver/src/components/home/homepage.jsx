39ebe8aecf0bdb369c1668f4bf500868161d0169
Mutombe <simbarashemutombe1@gmail.com>
2024-11-09 14:53:27 +0200

    ...

--- silver/src/components/home/homepage.jsx
+++ silver/src/components/home/homepage.jsx
@@ -237,7 +237,7 @@ const Hero = () => {
             className="w-full max-w-md"
           >
             {/* Replace with your actual logo */}
-            <div className=" backdrop-blur-xl p-6 rounded-2xl mt-40">
+            <div className=" backdrop-blur-xl p-6 rounded-2xl mt-40 mb-40">
               <img
                 src="/silver-logo.svg"
                 alt="Leaf"
