4135a333395716f6fbba0cb06ed1abcdb6c5982e
Mutombe <simbarashemutombe1@gmail.com>
2024-11-12 08:45:15 +0200

    ...

--- silver/src/components/projects/projects.jsx
+++ silver/src/components/projects/projects.jsx
@@ -53,7 +53,7 @@ const Projects = () => {
             transition={{ duration: 0.6, delay: 0.2 }}
             className="mt-3 max-w-md mx-auto text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
           >
-            Desclaimer: <br></br><strong style={{textEmphasisColor: "red",}} className='text-red-500'>The projects content is being updated.</strong>
+            Desclaimer: <br></br><strong style={{textEmphasisColor: "red",}} className=''>The projects content is being updated.</strong>
           </motion.p>
         </div>
         {/*<div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
